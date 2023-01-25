d0e570efef8182ec3feaa8e7d2837eedadc67a0b
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-25 19:52:47 -0300

    ejs

--- Projeto01/index.js
+++ Projeto01/index.js
@@ -4,8 +4,10 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get("/",(req, res)=>{
+    let nome = 'Mateus Godoi'
+    let lang = 'Java'
     //res.render("index.ejs")
-    res.render("principal/perfil")
+    res.render("index")
 })
 
 app.listen(8080,()=>{
